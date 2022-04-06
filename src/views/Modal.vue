7bc94a7fe1340b2adfec6b07a97aa7431d582d25
hasanghafuri <hasanghafuri00@gmail.com>
2022-04-06 22:30:13 +0430

    end rocket

---
+++ src/views/Modal.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-show="showback" class="backdrop">
+    <div v-show="show" class="modal">
+      <div>
+        <img @click="off" src="@/assets/icon/close.png/" alt="" class="" />
+      </div>
+      <div class="flex flex-col gap-2 mt-5">
+        <p class="bg-gray-300">
+          {{ selctedarticle.body }}
+        </p>
+        <span class="bg-gray-300">
+          {{ selctedarticle.id }}
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "modal",
+  data() {
+    return {};
+  },
+  props: {
+    showback: {
+      type: Boolean,
+    },
+    show: {
+      type: Boolean,
+    },
+    selctedarticle: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  methods: {
+    off() {
+      this.$emit("offpop");
+    },
+  },
+};
+</script>
+<style scoped>
+.modal {
+  @apply bg-white text-black top-1/2  left-1/4  py-3 px-3 w-80   h-60  text-center   rounded-lg fixed z-50;
+}
+.backdrop {
+  @apply w-full h-full fixed  top-0 left-0 right-0 bottom-0 bg-gray-800  backdrop-filter isolate opacity-90  z-30;
+}
+.modal {
+  top: 350px;
+  left: 700px;
+}
+</style>
--- src/views/list.vue
+++ src/views/list.vue
@@ -1,24 +1,43 @@
 <template>
   <div>
     <div class="content mt-6 flex flex-wrap gap-28">
-      <div v-for="(article, index) in articles" :key="index" class="card">
+      <div
+        @click="sendArticleData(article)"
+        v-for="(article, index) in articles"
+        :key="index"
+        class="card"
+      >
         <div></div>
         <span class="userid">userid:{{ article.id }} </span>
         <h1 class="title">{{ article.title }}</h1>
         <p class="body">{{ article.body }}</p>
       </div>
     </div>
+    <Modal
+      :selctedarticle="selctedarticle"
+      :show="show"
+      @offpop="offpop"
+      :showback="showback"
+    />
   </div>
 </template>
 <script>
 import axios from "axios";
+import Modal from "@/views/Modal.vue";
 export default {
   name: "list",
+  components: {
+    Modal,
+  },
   data() {
     return {
       articles: [],
+      selctedarticle: {},
+      show: false,
+      showback: false,
     };
   },
+
   mounted() {
     this.getArticleById();
   },
@@ -28,12 +47,21 @@ export default {
         this.articles = res.data.slice(90);
       });
     },
+    sendArticleData(article) {
+      this.selctedarticle = article;
+      this.show = true;
+      this.showback = true;
+    },
+    offpop() {
+      this.show = false;
+      this.showback = false;
+    },
   },
 };
 </script>
 <style scoped>
 .card {
-  @apply bg-gray-200  rounded-lg py-6 px-6 w-1/4 flex flex-col items-center  gap-2;
+  @apply bg-gray-200 z-0  rounded-lg py-6 px-6 w-1/4 flex flex-col items-center  gap-2;
 }
 .userid {
   @apply bg-gray-100 py-2 px-2 text-center w-60;
